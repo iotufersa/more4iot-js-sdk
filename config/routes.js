a789f575e32c199b57b0763ffc399d62a22b20bb
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2021-05-29 09:00:31 -0300

    refactor: device to resource

--- config/routes.js
+++ config/routes.js
@@ -9,38 +9,18 @@ const actionManagerRouteGetActionsByUuid = 'actions';
 const actionManagerRouteGetAll = 'actions';
 const actionManagerRouteNotifyActionCommunicator = 'actions/notify';
 
-const deviceManagerRouteSave = 'devices/inscribe';
-const deviceManagerRouteUpdate = 'devices/update';
-const deviceManagerRouteGetAll = 'devices';
-const deviceManagerRouteFindDevice = 'devices';
-const deviceManagerRouteCheckDevice = 'devices/verify';
-const deviceManagerRouteDelete = 'devices/delete';
+const deviceManagerRouteSave = 'resources/inscribe';
+const deviceManagerRouteUpdate = 'resources/update';
+const deviceManagerRouteGetAll = 'resources';
+const deviceManagerRouteFind = 'resources';
+const deviceManagerRouteCheck = 'resources/verify';
+const deviceManagerRouteDelete = 'resources/delete';
 
 const dataManagerRouteSave = 'datas/persist';
 const dataManagerRouteGetDataByUuid = 'datas';
 const dataManagerRouteGetAll = 'datas';
 const dataManagerRouteGetLastByUuid = 'datas/last';
 const dataManagerRouteDelete = 'datas/delete';
-
-const req_deviceManagerRouteSave = 'devices/inscribe';
-const req_deviceManagerRouteUpdate = 'devices/update';
-const req_deviceManagerRouteGetAll = 'devices';
-const req_deviceManagerRouteFindDevice = 'devices';
-const req_deviceManagerRouteCheckDevice = 'devices/verify';
-const req_deviceManagerRouteDelete = 'devices/delete';
-
-const req_dataManagerRouteSave = 'datas/persist';
-const req_dataManagerRouteGetDataByUuid = 'datas';
-const req_dataManagerRouteGetAll = 'datas';
-const req_dataManagerRouteGetLastByUuid = 'datas/last';
-const req_dataManagerRouteDelete = 'datas/delete';
-
-const req_actionManagerRouteSave = 'actions/inscribe';
-const req_actionManagerRouteGetActionsByUuid = 'actions';
-const req_actionManagerRouteGetAll = 'actions';
-const req_actionManagerRouteNotifyActionCommunicator = 'actions/notify';
-
-const req_actionCommunicatorRouteNotify = 'actionCommunicator/notify';
 
 const inputCommunicatorRoute = 'inputCommunicator';
 
@@ -55,34 +35,14 @@ exports.deviceManagerRouteSave = deviceManagerRouteSave;
 exports.deviceManagerRouteUpdate = deviceManagerRouteUpdate;
 exports.deviceManagerRouteDelete = deviceManagerRouteDelete;
 exports.deviceManagerRouteGetAll = deviceManagerRouteGetAll;
-exports.deviceManagerRouteFindDevice = deviceManagerRouteFindDevice;
-exports.deviceManagerRouteCheckDevice = deviceManagerRouteCheckDevice;
+exports.deviceManagerRouteFindDevice = deviceManagerRouteFind;
+exports.deviceManagerRouteCheckDevice = deviceManagerRouteCheck;
 
 exports.dataManagerRouteSave = dataManagerRouteSave;
 exports.dataManagerRouteGetDataByUuid = dataManagerRouteGetDataByUuid;
 exports.dataManagerRouteGetAll = dataManagerRouteGetAll;
 exports.dataManagerRouteGetLastByUuid = dataManagerRouteGetLastByUuid;
 exports.dataManagerRouteDelete = dataManagerRouteDelete;
-
-exports.req_actionManagerRouteSave = req_actionManagerRouteSave;
-exports.req_actionManagerRouteGetActionsByUuid = req_actionManagerRouteGetActionsByUuid;
-exports.req_actionManagerRouteGetAll = req_actionManagerRouteGetAll;
-exports.req_actionManagerRouteNotifyActionCommunicator = req_actionManagerRouteNotifyActionCommunicator;
-
-exports.req_deviceManagerRouteSave = req_deviceManagerRouteSave;
-exports.req_deviceManagerRouteUpdate = req_deviceManagerRouteUpdate;
-exports.req_deviceManagerRouteDelete = req_deviceManagerRouteDelete;
-exports.req_deviceManagerRouteGetAll = req_deviceManagerRouteGetAll;
-exports.req_deviceManagerRouteFindDevice = req_deviceManagerRouteFindDevice;
-exports.req_deviceManagerRouteCheckDevice = req_deviceManagerRouteCheckDevice;
-
-exports.req_dataManagerRouteSave = req_dataManagerRouteSave;
-exports.req_dataManagerRouteGetDataByUuid = req_dataManagerRouteGetDataByUuid;
-exports.req_dataManagerRouteDelete = req_dataManagerRouteDelete;
-exports.req_dataManagerRouteGetAll = req_dataManagerRouteGetAll;
-exports.req_dataManagerRouteGetLastByUuid = req_dataManagerRouteGetLastByUuid;
-
-exports.req_actionCommunicatorRouteNotify = req_actionCommunicatorRouteNotify;
 
 exports.inputCommunicatorRoute = inputCommunicatorRoute;
 
--- config/services.js
+++ config/services.js
@@ -1,6 +1,6 @@
 module.exports = {
   ROUTE_SWAGGER_API: "api/swagger",
-  DEVICE_MANAGER_NAME: "device-manager",
+  RESOURCE_MANAGER_NAME: "resource-manager",
   DATA_MANAGER_NAME: "data-manager",
   ACTION_MANAGER_NAME: "action-manager",
   ACTION_COMMUNICATOR_NAME: "action-communicator",
